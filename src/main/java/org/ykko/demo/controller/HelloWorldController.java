8c31c5976f31a3b23870df2d16577c470bc50958
YK <ykko21@gmail.com>
2025-06-25 23:55:19 -0400

    init

---
+++ src/main/java/org/ykko/demo/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package org.ykko.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+    @GetMapping()
+    public String message() {
+        return "Hello World";
+    }
+}
